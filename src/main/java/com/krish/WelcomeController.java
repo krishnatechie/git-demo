a488c4f3640d9b4d7e1064bf8012759de66112f6
Krishna Arigela <krishna2439.techie@gmail.com>
2022-11-10 18:58:51 +0530

    Krish: Resolving Conflict while Dashboard add

--- src/main/java/com/krish/WelcomeController.java
+++ src/main/java/com/krish/WelcomeController.java
@@ -3,7 +3,7 @@ package com.krish;
 public class WelcomeController {
 	
     public String welcome() {
-        return "This is Swetha I`m great! However hes also great because husband. Hey we are happy...";
+        return "This is Swetha I`m great! However hes also great because husband. Hey we are happy... Thanks God we are in understanding now";
     }
     
     public String home() {
@@ -14,8 +14,9 @@ public class WelcomeController {
     	return "King.... No egos";
     }
     
-    public void dashboard() {
+    public String dashboard() {
     	//This return app dashboard
+    	return "dashboard.html";
     }
 
 }
