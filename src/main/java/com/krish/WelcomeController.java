627aa42401b2aa88b56dce6d8e0c41bb2189ced0
Krishna Arigela <krishna2439.techie@gmail.com>
2022-11-10 18:45:59 +0530

    Update ignore file

--- src/main/java/com/krish/WelcomeController.java
+++ src/main/java/com/krish/WelcomeController.java
@@ -3,7 +3,7 @@ package com.krish;
 public class WelcomeController {
 	
     public String welcome() {
-        return "This is Swetha I`m great! However hes also great because husband.";
+        return "This is Swetha I`m great! However hes also great because husband. Hey we are happy...";
     }
     
     public String home() {
