3c9a3350b4dc7d1a0a1d4a34750f4eee82aacd8a
Krishna Arigela <krishna2439.techie@gmail.com>
2022-11-10 18:32:09 +0530

    Swetha: Leaving egos

--- src/main/java/com/krish/WelcomeController.java
+++ src/main/java/com/krish/WelcomeController.java
@@ -3,7 +3,7 @@ package com.krish;
 public class WelcomeController {
 	
     public String welcome() {
-        return "This is Swetha I`m great!";
+        return "This is Swetha I`m great! However hes also great because husband.";
     }
     
     public String home() {
@@ -11,7 +11,7 @@ public class WelcomeController {
     }
     
     public String ego() {
-    	return "King....";
+    	return "King.... No egos";
     }
 
 }
